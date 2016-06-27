e2d7626d7106315ed20d1cf731fc31997a53a9ca
Pascal-W <p.wegner1992@gmx.de>
2016-06-27 23:14:14 +0200

    CleanUp + HTML improved

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ $(document).ready(function () {
                 $('#logo').css("width", "300px");
                 $('#logo').css("margin-top", "20px");
                 $('nav ul').css("margin-top", "0px");
-                $('.current').css("padding-bottom", "50px");
+                $('.current').css("padding-bottom", "52px");
             } else {
                 $('header').css("height", "70px");
                 $('#content').css("margin-top", "70px");
                 $('#logo').css("width", "10%");
                 $('#logo').css("margin-top", "5px");
-                $('nav ul').css("margin-top", "-20px");
-                $('.current').css("padding-bottom", "0px");
+                $('nav ul').css("margin-top", "-30px");
+                $('.current').css("padding-bottom", "12px");
             }
 
         } else {
@@ -24,7 +24,7 @@ $(document).ready(function () {
             $('#logo').css("width", "100px");
             $('#logo').css("margin-top", "10px");
             $('nav ul').css("margin-top", "0px");
-            $('.current').css("padding-bottom", "50px");
+            $('.current').css("padding-bottom", "52px");
         }
     });
     $(window).scroll(function () {
@@ -35,8 +35,8 @@ $(document).ready(function () {
                 $('#content').css("margin-top", "70px");
                 $('#logo').css("width", "10%");
                 $('#logo').css("margin-top", "5px");
-                $('nav ul').css("margin-top", "-20px");
-                $('.current').css("padding-bottom", "0px");
+                $('nav ul').css("margin-top", "-30px");
+                $('.current').css("padding-bottom", "12px");
             } else {
                 smallHeader = false;
                 $('header').css("height", "140px");
@@ -44,7 +44,13 @@ $(document).ready(function () {
                 $('#logo').css("width", "300px");
                 $('#logo').css("margin-top", "20px");
                 $('nav ul').css("margin-top", "0px");
-                $('.current').css("padding-bottom", "50px");
+                $('.current').css("padding-bottom", "52px");
+            }
+        }else{
+            if ($(this).scrollTop() > 0) {
+                smallHeader = true;
+            } else {
+                smallHeader = false;
             }
         }
 
